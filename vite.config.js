872b7827d69ebb2817b8fcb3f487dc3b341ba6cf
YI5531 <156794621+YI5531@users.noreply.github.com>
2025-11-22 14:06:35 +0800

    Set base path for Android container compatibility
    Set base path to '/' for correct root file resolution in Android container.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 增加 base: '/' 确保 Android 容器能正确找到根文件
+  base: '/',
   build: {
     outDir: 'dist',
   }
